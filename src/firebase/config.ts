dc90ff694b670f0d9545eb51342963354c789e4d
Akeprapu Dulyapurk <6638270221@student.chula.ac.th>
2025-10-16 08:25:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4973221782-1b55f",
+  "appId": "1:668042989889:web:9292b1a9adbacf7bcf91c0",
+  "apiKey": "AIzaSyAmt2dHz8ac5pPA7kwRMA_xDgd8UPn1s-M",
+  "authDomain": "studio-4973221782-1b55f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "668042989889"
+};
