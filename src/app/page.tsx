cd6512d3d85415ef4bf81042afe6fd92a7e57b09
akeprapu.totte <akeprapu.totte@gmail.com>
2025-10-16 14:35:51 +0000

    lk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
-'use client';
+'use client'
 import { ReservationsPage } from '@/components/reservations/reservations-page';
+import { useMemoFirebase } from "@/firebase/provider";
+import { collection, query, orderBy } from "firebase/firestore";
+import { useCollection, useFirestore } from "@/firebase";
+import type { Reservation } from "@/lib/types";
 
 export default function Home() {
+  const firestore = useFirestore();
+
+  const reservationsQuery = useMemoFirebase(() => {
+    if (!firestore) return null;
+    return query(collection(firestore, "reservations"), orderBy("date", "desc"));
+  }, [firestore]);
+
+  const { data: reservations, isLoading } = useCollection<Reservation>(reservationsQuery);
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <div className="container mx-auto px-4 md:px-6">
-      <ReservationsPage />
+    <div className="w-full mx-auto px-4 sm:px-6 md:px-8">
+      <ReservationsPage reservations={reservations || []} />
     </div>
   );
 }
