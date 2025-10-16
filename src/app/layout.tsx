1701de286ca034f90bdc7c29064ab5fbe0933c8c
Akeprapu Dulyapurk <6638270221@student.chula.ac.th>
2025-10-16 08:13:35 +0000

    commonroom booking is too left

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <div className="relative flex min-h-screen flex-col">
           <Header />
-          <main className="flex-1">{children}</main>
+          <main className="flex-1 container">{children}</main>
         </div>
         <Toaster />
       </body>
